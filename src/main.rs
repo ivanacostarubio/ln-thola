5c3386d274529a36e86c64a8155d9cf0d852aa45
Ivan Acosta-Rubio <ivan@softwarecriollo.com>
2019-03-15 09:33:38 -0600

    Removing unwraps. Thanks you sebasmagri for coaching me

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ extern crate thola;
 extern crate threadpool;
 
 use threadpool::ThreadPool;
+
 use std::sync::mpsc::channel;
+
+use std::net::SocketAddr;
 
 
 use grpc::RequestOptions;
@@ -16,47 +19,79 @@ use thola::macaroon_data::MacaroonData;
 use thola::rpc;
 
 
+fn load_certificate() -> TLSCertificate{
+
+    if let Some(cert_filename) = std::env::args().into_iter().skip(2).next(){
+        if let Ok(r) = TLSCertificate::from_path(cert_filename){
+            return r;
+        }else{
+            panic!("Error loading Certificate");
+        }
+    }else{
+        panic!("Error loading Certificate");
+    } 
+}
+
 fn generate_client() -> LightningClient {
 
-    use std::net::SocketAddr;
 
-    let certificate = {
-        let cert_filename = std::env::args()
-            .into_iter().skip(2).next()
-            .unwrap();
-        TLSCertificate::from_path(cert_filename)
-            .unwrap()
+    
+    let socket_addr_string = match std::env::args().into_iter().skip(4).next(){
+        Some(s) => s,
+        None => String::from("127.0.0.1:10009")
     };
 
 
-    let default = "127.0.0.1:10009";
-    let socket_addr_string = std::env::args()
-        .into_iter().skip(4).next()
-        .unwrap_or(default.to_owned());
-    let socket_addr: SocketAddr = socket_addr_string.parse().unwrap();
-    let host = socket_addr.ip().to_string();
-    let conf = Default::default();
+    if let Ok(socket_addr) = socket_addr_string.parse::<SocketAddr>(){
+        let host = socket_addr.ip().to_string();
+        let conf = Default::default();
 
-    let tls = certificate.into_tls(host.as_str())
-        .unwrap();
-    let c = grpc::Client::new_expl(&socket_addr, host.as_str(), tls, conf)
-        .unwrap();
-    return LightningClient::with_client(c)
+
+
+        let certificate = load_certificate();
+
+
+        if let Ok(tls) = certificate.into_tls(host.as_str()){
+
+            if let Ok(c) = grpc::Client::new_expl(&socket_addr, host.as_str(), tls, conf){
+
+                return LightningClient::with_client(c)
+
+            }else{
+
+                panic!("Could not create LightningClient Connection");
+
+            };
+
+        }else{
+            panic!("Could not create TLS");
+        };
+
+    }else{
+        panic!("Could not parse lnd host. Please use IP:PORT");
+    }
+
+
 }
 
 
 
 fn metadata() -> RequestOptions {
-    let macaroon_data = {
-        let macaroon_file_path = std::env::args()
-            .into_iter().skip(3).next()
-            .unwrap();
-        MacaroonData::from_file_path(macaroon_file_path)
-            .unwrap()
+
+    if let Some(macaroon_file_path) = std::env::args().into_iter().skip(3).next(){
+
+        if let Ok(macaroon_data) = MacaroonData::from_file_path(macaroon_file_path){
+            return RequestOptions { metadata: macaroon_data.metadata(), };
+        }else{
+            panic!("Error loading macaroon");
+        };
+    }else{
+        panic!("Error loading macaroon");
     };
-    
-    return RequestOptions { metadata: macaroon_data.metadata(), };
+
 }
+
+
 
 fn my_query_routes(satoshis: i64, remote_node: &rpc::LightningNode) ->bool { 
 
@@ -80,6 +115,38 @@ fn my_query_routes(satoshis: i64, remote_node: &rpc::LightningNode) ->bool {
 
 }
 
+fn parse_sats_amount() -> i64 {
+
+    if let Some(satoshis_env) = std::env::args().into_iter().skip(1).next(){
+
+      if let Ok(satoshis) = satoshis_env.parse::<i64>(){
+          return satoshis
+      }else{
+        panic!("Error parsing the satoshi amount");
+      }
+    }else{
+        panic!("Error parsing the satoshi amount");
+
+    }
+}
+
+fn query_graph() -> rpc::ChannelGraph{
+
+
+    let client = generate_client();
+
+    let graph_req = rpc::ChannelGraphRequest::new();
+    let graph_resp = client.describe_graph( metadata(), graph_req); 
+ 
+    if let Ok(graph) = graph_resp.wait(){
+        return graph.1;
+    }else{
+        panic!("Error downloading graph");
+    }
+}
+
+
+
 fn main() {
 
     if std::env::args().len() < 5 {
@@ -92,23 +159,19 @@ fn main() {
 
     println!("Downloading Graph");
 
+    let graph = query_graph();
 
-    let satoshis_env = std::env::args().into_iter().skip(1).next().unwrap();
-    let satoshis = satoshis_env.parse::<i64>().unwrap();
-    let client = generate_client();
-    let graph_req = rpc::ChannelGraphRequest::new();
-    let graph_resp = client.describe_graph( metadata(), graph_req); 
-    let ww = graph_resp.wait().unwrap();
-
-
-    let count = ww.1.nodes.len() as usize;
-    let ccc = ww.1.nodes.clone();
+    let count = graph.nodes.len() as usize;
+    let ccc = graph.nodes.clone();
 
     let n_workers = 100;
-    let n_jobs = ww.1.nodes.len();
+    let n_jobs = graph.nodes.len();
     let pool = ThreadPool::new(n_workers);
 
     let (tx,rx) = channel();
+
+
+    let satoshis = parse_sats_amount();
 
     for node in ccc.into_iter() {
 
